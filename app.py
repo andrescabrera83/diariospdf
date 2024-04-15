567baeae9612874e232423bd63dfbfe5424c67de
Andres Cabrera <andresexplorer83@gmail.com>
2024-04-15 00:52:54 -0300

    last update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_file,
 import os
 import fitz
 from werkzeug.utils import secure_filename
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -21,6 +22,25 @@ def get_highlighted_pdf_files():
     pdf_files = [f for f in os.listdir(pdf_folder) if f.startswith('highlighted_') and f.endswith('.pdf')]
     return pdf_files
 
+def extract_text_from_all_columns(file_path):
+    try:
+        # Load the Excel file into a pandas DataFrame
+        df = pd.read_excel(file_path, engine='openpyxl')  # Specify the engine as 'openpyxl'
+        
+        # Initialize an empty list to store all text
+        all_text = []
+
+        # Iterate through all columns in the DataFrame
+        for column in df.columns:
+            # Extract text from the current column and append it to the all_text list
+            text_list = df[column].astype(str).tolist()
+            all_text.extend(text_list)
+
+        return all_text
+    except Exception as e:
+        return str(e)
+
+
 # Route to render the upload form
 @app.route('/')
 def index():
@@ -37,6 +57,9 @@ def display_pdf():
     pdf_folder = app.config['UPLOAD_FOLDER']
     return send_from_directory(pdf_folder, pdf_filename)
 
+
+#######################################################################################################################
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # Check if the post request has the file part
@@ -45,10 +68,40 @@ def upload_file():
     
     # Get the list of uploaded files
     files = request.files.getlist('file')
+
+    filesexcel = request.files.getlist('fileexcel')
+    
+
+    for fileexcel in filesexcel:
+        if fileexcel.filename == '':
+            continue
+        
+        if fileexcel:
+            # Save the file to a temporary directory
+            filename = secure_filename(fileexcel.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            fileexcel.save(file_path)
+
+            
+            # Extract text from the Excel file
+            column_name = request.form.get('column_name')
+            text_list = extract_text_from_all_columns(file_path)
+
+            print(text_list)
+            
+            # Return the extracted text as a downloadable text file
+            output_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'extracted_text.txt')
+            with open(output_file_path, 'w') as f:
+                for text in text_list:
+                    f.write(text + '\n')
+            
+            return send_file(output_file_path, as_attachment=True, download_name='extracted_text.txt', mimetype='text/plain')
+        else:
+            return 'Invalid file format'
     
     # Get the value from the textarea
-    textarea_value = request.form.get('palavras')
-    words_to_highlight = textarea_value.split()
+    words_to_highlight = request.form.get('palavras')
+    #words_to_highlight = textarea_value.split()
     
     for file in files:
         if file.filename == '':
@@ -65,14 +118,12 @@ def upload_file():
             
             # Iterate through each page in the PDF
             for page in doc:
-                # Iterate through each word to highlight
-                for word in words_to_highlight:
-                    # Search for the word on the page
-                    text_instances = page.search_for(word)
-                    # Highlight the word
-                    for inst in text_instances:
-                        highlight = page.add_highlight_annot(inst)
-                        highlight.update()
+               
+                text_instances = page.search_for(words_to_highlight,  quads=True)
+                # Highlight the word
+                for inst in text_instances:
+                    highlight = page.add_highlight_annot(inst)
+                    highlight.update()
             
             # Save the modified PDF with highlights
             output_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'highlighted_' + filename)
